8f8a195f7a29c1d7cb9c754d6a1ec6cd24d7a085
mnp1048 <mnp1048@gmail.com>
2020-05-07 17:51:37 +0900

    200507 commit

--- Jungol_기초다지기/src/배열1/자가진단06/Main.java
+++ Jungol_기초다지기/src/배열1/자가진단06/Main.java
@@ -6,17 +6,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] array = new int[10] ;
-		int min = 0;
-		for (int i = 0; i < array.length; i++) {
-			array[i]=sc.nextInt();
-			if(array[i] > array[i+1]) {
-				min = array[i+1];
-			}else if(array[i] <= array[i+1]) {
-				min = array[i];
-			}
-			System.out.println(min);
+		int [] num = new int [10];
+		int [] inputNum = new int[num.length];
+		for (int i = 0; i < num.length; i++) {
+			inputNum[i] = sc.nextInt();
+			num[i] = inputNum[i];
 		}
+		
+		int numMin = 1000;
+		for (int i = 0; i < inputNum.length; i++) {
+			
+			if(num[i] < numMin) {
+				numMin = num[i];
+			} else {
+				continue;
+			}
+			
+		}
+		
+		System.out.println(numMin);
+		
+		sc.close();
+
 	}
 
 }
