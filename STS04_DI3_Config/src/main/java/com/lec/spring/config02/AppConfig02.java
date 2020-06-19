907bef2a6b0c6495c1e10691a1c431e7518f6ed8
mnp1048 <mnp1048@gmail.com>
2020-06-19 17:10:57 +0900

    spring annotation

---
+++ STS04_DI3_Config/src/main/java/com/lec/spring/config02/AppConfig02.java
@@ -0,0 +1,22 @@
+package com.lec.spring.config02;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.lec.beans.*;
+//Java class를 이용한 설정
+//class name 앞에 반드시 어노테이션 명시 필요
+//@Configuration --> 이 클래스는 '스프링 설정'에 사용되는 클래스입니다.
+//결국 컨테이너 역할을 한다.
+public class AppConfig02 {
+	
+	@Bean
+	public Score score1() { //메소드 이름 score1이 bean의 name 값 (id 값)이 된다.
+		return new Score(100,85,75,"좋아요");
+	}
+	
+	@Bean
+	public Student stu1() {
+		return new Student("훈지예지",34,score1());
+	}
+
+}
---
+++ STS06_PropertySource/src/main/java/com/lec/spring/profile4/xml/ProfileMain4.java
@@ -0,0 +1,26 @@
+package com.lec.spring.profile4.xml;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class ProfileMain4 {
+
+	public static void main(String[] args) {
+		String config = "run";
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		
+		//현재 활성화 할 profile이 무엇인지 setting 필요
+		ctx.getEnvironment().setActiveProfiles(config);
+		
+		//해당 profile의 설정 파일만 설정 됨.
+		ctx.load("appCtx4_dev.xml","appCtx4_run.xml");
+		ctx.refresh();
+		
+		ServiceInfo info = ctx.getBean("serverInfo",ServiceInfo.class);
+		System.out.println("ip: "+info.getIpNum());
+		System.out.println("port: "+info.getPortNum());
+		
+		ctx.close();
+	}
+
+}
